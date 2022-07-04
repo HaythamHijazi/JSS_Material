ff33827bc9bf8acb0417cf30093226f6cdc1d999
Haytham Hijazi <59981239+HaythamHijazi@users.noreply.github.com>
2022-07-05 01:39:47 +0300

    Update verifying_triggers.m

--- verifying_triggers.m
+++ verifying_triggers.m
@@ -3,7 +3,7 @@
 eeglab
 clear all; close all; clc
 
-cd('C:\Users\JulioMedeiros\Desktop\analysis\importfiles\triggers')
+ 
 
 files=dir('*.set');
 
@@ -17,7 +17,7 @@ for i=1:length(files)
     
     name_file=files(i).name;
     EEG.etc.eeglabvers = '14.1.2'; % this tracks which version of EEGLAB is being used, you may ignore it
-    EEG = pop_loadset('filename',name_file,'filepath','C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\');
+    EEG = pop_loadset('filename',name_file,'filepath',' ');
     EEG = eeg_checkset( EEG );
     
     if sum(ismember({EEG.event.type},'99'))==0
@@ -38,5 +38,4 @@ for i=1:length(files)
     end
 end
 
-% save('C:\Users\JulioMedeiros\Desktop\triggersinfor','container_infor','container_99_files','container_15_files')
-
+ 
