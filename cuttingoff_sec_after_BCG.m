de52c48aaa77a4816118419526695ceed6519eaa
Haytham Hijazi <59981239+HaythamHijazi@users.noreply.github.com>
2022-07-05 04:33:14 +0300

    Update cuttingoff_sec_after_BCG.m

--- cuttingoff_sec_after_BCG.m
+++ cuttingoff_sec_after_BCG.m
@@ -1,8 +1,6 @@
-% EEGLAB history file generated on the 16-Sep-2020
 % ------------------------------------------------
 EEG.etc.eeglabvers = '14.1.2'; % this tracks which version of EEGLAB is being used, you may ignore it
 
-cd('C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG\\')
 
 files=dir('*.set');
 
@@ -10,7 +8,7 @@ files=dir('*.set');
 
 for i=1:length(files)
     filename=files(i).name;
-    EEG = pop_loadset('filename',filename,'filepath','C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG');
+    EEG = pop_loadset('filename',filename,'filepath','');
     %     idx_start=find(strcmp({EEG.event.type},'99'));
     %     time_start=(EEG.event(idx_start).latency)/EEG.srate;
     %     EEG = pop_select( EEG,'notime',[0 time_start] );
@@ -26,7 +24,7 @@ for i=1:length(files)
     EEG = eeg_checkset( EEG );
     EEG.setname=strcat(filename(1:end-4),'_BCG_fixed');
     EEG = eeg_checkset( EEG );
-    EEG = pop_saveset( EEG, 'filename',strcat(EEG.setname,'.set'),'filepath','C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG\\fixed\\');
+    EEG = pop_saveset( EEG, 'filename',strcat(EEG.setname,'.set'),'filepath','');
     EEG = eeg_checkset( EEG );
 end
 
