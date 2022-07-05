4b20eb4490269c5cec05195d56e8f01e7ac897ff
Haytham Hijazi <59981239+HaythamHijazi@users.noreply.github.com>
2022-07-05 04:36:25 +0300

    Update filtering.m

--- filtering.m
+++ filtering.m
@@ -1,14 +1,13 @@
 EEG.etc.eeglabvers = '14.1.2'; % this tracks which version of EEGLAB is being used, you may ignore it
 
-cd('C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG\\fixed\\')
-
+ 
 files=dir('*.set');
 
 for i=1:length(files)
     filename=files(i).name;
-    EEG = pop_loadset('filename',filename,'filepath','C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG\\fixed\\');
+    EEG = pop_loadset('filename',filename,' ');
     EEG = eeg_checkset( EEG );
-    EEG=pop_chanedit(EEG, 'lookup','C:\\Users\\JulioMedeiros\\Desktop\\Study1_EEG\\eeglab14_1_2b\\plugins\\dipfit2.3\\standard_BESA\\standard-10-5-cap385.elp');
+    EEG=pop_chanedit(EEG, 'lookup',' ');
     EEG = eeg_checkset( EEG );
     EEG = pop_eegfiltnew(EEG, [],1,[],1,[],0);
     EEG = eeg_checkset( EEG );
@@ -18,6 +17,6 @@ for i=1:length(files)
     EEG = pop_select( EEG,'nochannel',{'M2' 'M1' 'VEOG' 'EKG' 'PulseOx' 'Trigger'});
     EEG = eeg_checkset( EEG );
     EEG.setname=strcat(filename(1:end-4),'_filtered_1_45hz');
-    EEG = pop_saveset( EEG, 'filename',strcat(EEG.setname,'.set'),'filepath','C:\\Users\\JulioMedeiros\\Desktop\\analysis\\importfiles\\triggers\\GA\\1000QRS\\BCG\\fixed\\filtered');
+    EEG = pop_saveset( EEG, 'filename',strcat(EEG.setname,'.set'),'filepath',' ');
     EEG = eeg_checkset( EEG );
 end
